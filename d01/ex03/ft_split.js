a05bcfce5b1e764b7339ec1b45b6e20644323a9b
kabascolby <kabascolby@gmail.com>
2019-06-13 10:44:47 -0700

    create main for ex03

--- d01/ex03/ft_split.js
+++ d01/ex03/ft_split.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
 var ft_split = function (arg) {
-	if (typeof arg === 'string')
-		return arg.match(/[^\s]+/g).sort();
+	if (typeof arg === 'string'){
+		let str = "";
+		arg.match(/[^\s]+/g).sort()
+		.forEach(x => str = str + x + "\n");
+		return str.trim();
+	}
 	else
 		process.exit();
 }
---
+++ d01/ex03/main.js
@@ -0,0 +1,4 @@
+let ft_split = require('./ft_split');
+
+
+console.log(ft_split("Hello World AAA"));
